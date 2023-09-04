b070217fbc024fe2a80c943e6a39c1050e53ac6c
Duong Quoc Viet <ntysux@gmail.com>
2023-09-05 01:40:02 +0700

    create & using: signin ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Signin from "@/components/signin"
+
 export default function InitPage() {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen">
         <div className="space-y-3">
           <h2>Menoú</h2>
-          <button>Đăng nhập</button>
+          <Signin />
           <button>Tạo tài khoản</button>
         </div>
       </div>
