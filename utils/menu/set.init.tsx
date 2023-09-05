06c59d05d92faf4890235cfd2b3f94152bed1924
Duong Quoc Viet <ntysux@gmail.com>
2023-09-05 20:46:51 +0700

    handle set init menu

--- utils/menu/set.init.tsx
+++ utils/menu/set.init.tsx
@@ -1,6 +1,15 @@
 'use client'
 
+import { useAppDispatch } from "@/redux/hooks"
+import { initMenu } from "@/redux/menu/slice"
+import { useEffect } from "react"
+
 export default function SetInitMenuPages({children, pages}: {children: React.ReactNode, pages: any}) {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(initMenu(pages))
+  }, [])
 
   return <>{children}</>
 }
