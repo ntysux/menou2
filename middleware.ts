da7ad58d9503bc3a2c82352d3f2ad91e1d412281
Duong Quoc Viet <ntysux@gmail.com>
2023-09-05 13:08:23 +0700

    handle check token & next page

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,16 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+const url = process.env.NEXT_PUBLIC_APP_URL
+
 export default function Middleware(request: NextRequest) {
-  NextResponse.next()
+  const cookie = request.cookies.get('token')
+
+  if (cookie) {
+    return NextResponse.redirect(`${url}/menu`)
+  }
+  
+  return NextResponse.next()
 }
 
 export const config = {
