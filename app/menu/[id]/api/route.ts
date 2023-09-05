10824c72052a7fdb38db61e4d5a91cd776533804
Duong Quoc Viet <ntysux@gmail.com>
2023-09-06 01:30:17 +0700

    handle get page by param & show page

---
+++ app/menu/[id]/api/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
+import { Client } from "@notionhq/client"
+
+const notion = new Client({auth: process.env.NOTION_KEY})
+
+export async function GET(request: NextRequest, {params}: {params: {id: string}}) {
+  const {id} = params
+  const pageResponse: any = await notion.pages.retrieve({
+    page_id: id
+  })
+
+  const {
+    properties: {
+      name, materials, required, steps, status
+    }
+  } = pageResponse
+
+  return NextResponse.json({
+    name: name.rich_text[0].plain_text,
+    materials: materials.rich_text[0]?.plain_text,
+    required: required.rich_text[0]?.plain_text,
+    steps: steps.rich_text[0]?.plain_text,
+    status: status.checkbox
+  })
+}
--- app/menu/[id]/page.tsx
+++ app/menu/[id]/page.tsx
@@ -1,7 +1,49 @@
-export default function ViewPage() {
+const url = process.env.NEXT_PUBLIC_APP_URL
+
+async function getPage(id: string) {
+  const res = await fetch(`${url}/menu/${id}/api`, {next: {revalidate: 300}})
+  const rs = await res.json()
+
+  return rs
+}
+
+export default async function ViewPage({params}: {params: {id: string}}) {
+  const {id} = params
+  const rs = await getPage(id)
+
   return (
     <>
-      view page
+      <h2>{rs.name}</h2>
+      <h3 className="font-bold">Nguyên liệu</h3>
+      <ul>
+        {
+          rs.materials?.split('|').map((material: any, index: number) => 
+            <li key={index}>{material}</li> 
+          )
+          ??
+          <div>Trống</div>
+        }
+      </ul>
+      <h3 className="font-bold">Chuẩn bị</h3>
+      <ul>
+        {
+          rs.required?.split('|').map((require: any, index: number) => 
+            <li key={index}>{require}</li> 
+          )
+          ??
+          <div>Trống</div>
+        }
+      </ul>
+      <h3 className="font-bold">Chế biến</h3>
+      <ul>
+        {
+          rs.steps?.split('|').map((step: any, index: number) => 
+            <li key={index}>{step}</li> 
+          )
+          ??
+          <div>Trống</div>
+        }
+      </ul>
     </>
   )
 }
