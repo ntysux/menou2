73eb8e0b2ddc26b3e005ebb78d1159a48cc4fc1f
Duong Quoc Viet <ntysux@gmail.com>
2023-09-05 20:51:11 +0700

    show menu pages from state

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,34 +1,36 @@
+'use client'
+
+import { useAppSelector } from '@/redux/hooks'
 import Link from 'next/link'
 
-export default async function MenuPage() {
-  return <></>
-  // return !rs.error ? (
-  //   <>
-  //     <div className="w-screen max-w-5xl mx-auto">
-  //       <div>
-  //         <Link href='/menu/new'>
-  //           <button>Thêm mới</button>
-  //         </Link>
-  //       </div>
+export default function MenuPage() {
+  const pages = useAppSelector(state => state.menu)
 
-  //       <div className='mt-9'>
-  //         {
-  //           rs.menuPages.map((page: any, index: number) =>
-  //             !page.deleted &&
-  //             <div key={index} className='flex space-x-3'>
-  //               <h2>{page.name}</h2>
-  //               <button>Xóa</button>
-  //               <button>Sửa</button>
-  //               <Link href={`/menu/${page.id}`}>
-  //                 <button>Đầy đủ</button>
-  //               </Link>
-  //             </div>
-  //           )
-  //         }
-  //       </div>
-  //     </div>
-  //   </>
-  // ) : (
-  //   <>Lỗi: {rs.error}</>
-  // )
+  return (
+    <>
+      <div className="w-screen max-w-5xl mx-auto">
+        <div>
+          <Link href='/menu/new'>
+            <button>Thêm mới</button>
+          </Link>
+        </div>
+
+        <div className='mt-9'>
+          {
+            pages.map((page: any, index: number) =>
+              !page.deleted &&
+              <div key={index} className='flex space-x-3'>
+                <h2>{page.name}</h2>
+                <button>Xóa</button>
+                <button>Sửa</button>
+                <Link href={`/menu/${page.id}`}>
+                  <button>Đầy đủ</button>
+                </Link>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </>
+  )
 }
