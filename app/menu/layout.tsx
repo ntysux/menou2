7d87375fc35b5a44f5f8d5e8274bad9bec6bb328
Duong Quoc Viet <ntysux@gmail.com>
2023-09-05 21:46:33 +0700

    handle fetching in page

--- app/menu/layout.tsx
+++ app/menu/layout.tsx
@@ -1,22 +1,6 @@
 import Signin from "@/components/signin"
 import ReduxProviders from "@/redux/providers"
-import SetInitMenuPages from "@/utils/menu/set.init"
 import { cookies } from "next/headers"
-
-const url = process.env.NEXT_PUBLIC_APP_URL
-
-async function allMenuPagesById(cookie: any) {
-  const res = await fetch(`${url}/menu/api`, {
-    headers: {cookie: `token=${cookie?.value}`}
-  })
-  const rs = await res.json()
-
-  if (rs.menuPages) {
-    return rs.menuPages
-  }
-
-  return undefined
-}
 
 export default async function MenuLayout({
   children,
@@ -24,16 +8,13 @@ export default async function MenuLayout({
   children: React.ReactNode
 }) {
   const cookie = cookies().get('token')
-  const rs = await allMenuPagesById(cookie)
 
   return (
     <>
       {!cookie && <Signin />}
 
       <ReduxProviders>
-        <SetInitMenuPages pages={rs}>
-          {children}
-        </SetInitMenuPages>
+        {children}
       </ReduxProviders>
     </>
   )
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,5 +1,24 @@
 import MenuContent from "@/components/menu/content"
+import { cookies } from "next/headers"
 
-export default function MenuPage() {
+const url = process.env.NEXT_PUBLIC_APP_URL
+
+async function allMenuPagesById() {
+  const cookie = cookies().get('token')
+  const res = await fetch(`${url}/menu/api`, {
+    headers: {cookie: `token=${cookie?.value}`}
+  })
+  const rs = await res.json()
+
+  if (rs.menuPages) {
+    return rs.menuPages
+  }
+
+  return undefined
+}
+
+export default async function MenuPage() {
+  const rs = await allMenuPagesById()
+
   return <MenuContent />
 }
