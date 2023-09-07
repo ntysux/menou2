cd762fd418b3140f48e63d14248d372320d86b23
Duong Quoc Viet <ntysux@gmail.com>
2023-09-07 17:26:01 +0700

    call api for change color

--- components/menu/content.tsx
+++ components/menu/content.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { useEffect } from 'react'
-import { checkedMenu, initMenu, removeMenu, removeMultiMenu } from '@/redux/menu/slice'
+import { changeColorMultiMenu, checkedMenu, initMenu, removeMenu, removeMultiMenu } from '@/redux/menu/slice'
 import CUDialog from './crud/c.u.dialog'
 
 const url = process.env.NEXT_PUBLIC_APP_URL
@@ -23,6 +23,14 @@ async function handleDeleteMulti(idList: string[]) {
   })
 }
 
+async function handleChangeColorMulti(idList: string[], color: string) {
+  await fetch(`${url}/menu/api/update/multi/color`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({idList, color})
+  })
+}
+
 export default function MenuContent({pages}: {pages: any}) {
   const dispatch = useAppDispatch()
   const menu = useAppSelector(state => state.menu)
@@ -36,7 +44,7 @@ export default function MenuContent({pages}: {pages: any}) {
 
   return (
     <div className="w-screen max-w-5xl mx-auto">
-      <div className='space-x-3'>
+      <div className='flex space-x-3'>
         <CUDialog>
           {setOpen => 
             <button onClick={() => setOpen(true)}>
@@ -50,17 +58,25 @@ export default function MenuContent({pages}: {pages: any}) {
         }}>
           Xóa
         </button>
-        <button>
-          Đổi màu
-        </button>
+        <div className='flex space-x-3'>
+          {
+            ['bg-rose-400', 'bg-teal-400', 'bg-purple-400'].map((color, index) =>
+              <button key={index} onClick={() => {
+                dispatch(changeColorMultiMenu(color))
+                handleChangeColorMulti(idList, color)
+              }}>
+                <div className={`p-2 rounded-full ${color}`} />  
+              </button>
+            )
+          }
+        </div>
       </div>
 
       <div className='mt-9'>
         {
           menu.map((page: any, index: number) =>
             !page.deleted &&
-            <div key={index} className='flex items-center space-x-3'>
-
+            <div key={index} className={`flex items-center space-x-3 ${page.color}`}>
               <input 
                 type="checkbox"
                 className='appearance-none w-6 h-6 ring-1 ring-neutral-300 rounded-lg checked:ring-2 checked:ring-neutral-800' 
