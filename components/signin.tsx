3067d2e5301c6decf97cf39664be816abc708c35
Duong Quoc Viet <ntysux@gmail.com>
2023-09-09 02:04:27 +0700

    show spin when isSubmiting is true

--- components/signin.tsx
+++ components/signin.tsx
@@ -75,44 +75,57 @@ export default function Signin({
                       setSubmitting(false)
                     }}
                   >
-                    <Form>
-                      <ErrorMessageBox name='error' />
-                      <div className='relative'>
-                        <Field 
-                          type="text" 
-                          name="username" 
-                          placeholder="Tên đăng nhập"
-                          autoComplete="username"
-                          className="
-                            py-3 w-full outline-none rounded-none border-b-2 border-neutral-800
-                            text-sm text-neutral-800 font-medium
-                          "
-                        />
-                        <div className='absolute inset-y-0 right-0 flex items-center'>
-                          <ErrorMessageBox name='username' />
+                    {({isSubmitting}) => (
+                      <Form>
+                        <ErrorMessageBox name='error' />
+                        <div className='relative'>
+                          <Field 
+                            type="text" 
+                            name="username" 
+                            placeholder="Tên đăng nhập"
+                            autoComplete="username"
+                            className="
+                              py-3 w-full outline-none rounded-none border-b-2 border-neutral-800
+                              text-sm text-neutral-800 font-medium
+                            "
+                          />
+                          <div className='absolute inset-y-0 right-0 flex items-center'>
+                            <ErrorMessageBox name='username' />
+                          </div>
                         </div>
-                      </div>
-                      <div className='relative'>
-                        <Field 
-                          type="password" 
-                          name="password" 
-                          placeholder="Mật khẩu"
-                          autoComplete="current-password"
-                          className="
-                            py-3 w-full outline-none rounded-none border-b-2 border-neutral-800
-                            text-sm text-neutral-800 font-medium
-                          "
-                        />
-                        <div className='absolute inset-y-0 right-0 flex items-center'>
-                          <ErrorMessageBox name='password' />
+                        <div className='relative'>
+                          <Field 
+                            type="password" 
+                            name="password" 
+                            placeholder="Mật khẩu"
+                            autoComplete="current-password"
+                            className="
+                              py-3 w-full outline-none rounded-none border-b-2 border-neutral-800
+                              text-sm text-neutral-800 font-medium
+                            "
+                          />
+                          <div className='absolute inset-y-0 right-0 flex items-center'>
+                            <ErrorMessageBox name='password' />
+                          </div>
                         </div>
-                      </div>
-                      <div className='mt-9 text-center'>
-                        <button type='submit' className='outline-none text-sm text-neutral-800 font-bold'>
-                          Đăng nhập
-                        </button>
-                      </div>
-                    </Form>
+                        <div className='mt-9 text-center'>
+                          <button 
+                            type='submit' 
+                            className='outline-none text-sm text-neutral-800 font-bold disabled:opacity-40'
+                            disabled={isSubmitting}
+                          >
+                            {
+                              isSubmitting 
+                              ?
+                              <div className="h-5 w-5 animate-spin rounded-full border-4 border-white border-r-neutral-800" />
+                              :
+                              <>Đăng nhập</>
+                            }
+                          </button>
+                          
+                        </div>
+                      </Form>
+                    )}
                   </Formik>
                 </Dialog.Panel>
               </Transition.Child>
