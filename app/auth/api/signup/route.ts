0bd86bd61da4f5b39216ba556a01646bdc6e1e08
Duong Quoc Viet <ntysux@gmail.com>
2023-09-08 20:26:04 +0700

    handle signup api

---
+++ app/auth/api/signup/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
+import jwt from 'jsonwebtoken'
+import { genSalt, hash } from 'bcrypt'
+import { Client as NotionClient } from "@notionhq/client"
+
+const notion = new NotionClient({auth: process.env.NOTION_KEY})
+const databaseId = process.env.NOTION_ACCOUNT
+
+interface Props {
+  values: {
+    username: string
+    password: string
+    name: string
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const {values: {username, password, name}}: Props = await request.json()
+
+  // check username in database
+  const {results} = await notion.databases.query({
+    database_id: databaseId!,
+    filter: {
+      property: 'username',
+      rich_text: {
+        contains: username
+      }
+    }
+  })
+  
+  // return error if username already exists
+  if(results.length) {
+    return NextResponse.json({username: 'Tên đăng nhập đã tồn tại.'})
+  } else {
+    // hash password with Bcrypt
+    const salt: string = await genSalt(10)
+    const hashedPassword: string = await hash(password, salt)
+
+    // store account into Notion
+    const accountResponse: any = await notion.pages.create({
+      parent: {
+        database_id: databaseId!
+      },
+      properties: {
+        username: {
+          title: [{
+            text: {
+              content: username
+            }
+          }]
+        },
+        password: {
+          rich_text: [{
+            text: {
+              content: hashedPassword
+            }
+          }]
+        },
+        name: {
+          rich_text: [{
+            text: {
+              content: name
+            }
+          }]
+        }
+      }
+    })
+
+    const id: string = accountResponse.id
+    
+    const token: string = jwt.sign(
+      {id}, // payload
+      process.env.SECRET_KEY!, //signature
+      {algorithm: 'HS256'}
+    )
+
+    return NextResponse.json({id}, {status: 200, headers: {'Set-Cookie': `token=${token}; Path=/`}})
+  }
+}
