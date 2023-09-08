2690b2ca21a189204b34c864ef369e0c9545d691
Duong Quoc Viet <ntysux@gmail.com>
2023-09-08 21:42:00 +0700

    add custom box shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        'custombox': '-10px -10px 15px rgba(255,255,255,0.5),10px 10px 15px rgba(70,70,70,0.12);'
+      }
     },
   },
   plugins: [],
