876f62aa1c32d67f6d803c03ff9396758ca48ea5
Duong Quoc Viet <ntysux@gmail.com>
2023-09-09 02:10:44 +0700

    chore: set text center

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Signup from "@/components/signup"
 export default function InitPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="space-y-3">
-        <h1 className="text-2xl text-neutral-800 font-light tracking-wider sm:text-5xl">
+      <div className="space-y-3 text-center">
+        <h1 className="text-2xl text-neutral-800 font-light tracking-wider sm:text-3xl">
           Menoú
         </h1>
         <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
