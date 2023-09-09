058446651869c942d81aa945b12fc3b300db4f70
Duong Quoc Viet <ntysux@gmail.com>
2023-09-09 16:31:59 +0700

    create: nav

---
+++ components/nav.tsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <header className="sticky top-0 z-10 bg-sky-200">
+      <nav className="flex justify-end items-center space-x-9 py-5">
+        <h3 className="text-sm text-neutral-800 font-medium leading-none">
+          Cá nhân
+        </h3>
+        <h3 className="text-sm text-neutral-800 font-medium leading-none">
+          Cộng đồng
+        </h3>
+      </nav>
+    </header>
+  )
+}
