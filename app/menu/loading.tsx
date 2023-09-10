2c153c32cdebef8aaa6ca14d827de3c56e74dd05
Duong Quoc Viet <ntysux@gmail.com>
2023-09-11 00:12:03 +0700

    update ui loading

--- app/menu/loading.tsx
+++ app/menu/loading.tsx
@@ -1,3 +1,33 @@
 export default function Loading() {
-  return <>Loading</>
+  return (
+    <div className="my-3 mb-20 grid grid-cols-1 gap-3 sm:mb-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      <div className="shadow shadow-neutral-200 rounded-xl p-3">
+        <div className="animate-pulse space-y-7">
+          <div className="flex justify-between items-start">
+            <div className="rounded-md bg-neutral-200 px-14 py-2" />
+            <div className="rounded-md p-2.5 bg-neutral-200" />
+          </div>
+          <div className="px-9 py-3 rounded-md bg-neutral-200 w-fit" />
+        </div>
+      </div>
+      <div className="shadow shadow-neutral-200 rounded-xl p-3">
+        <div className="animate-pulse space-y-7">
+          <div className="flex justify-between items-start">
+            <div className="rounded-md bg-neutral-200 px-14 py-2" />
+            <div className="rounded-md p-2.5 bg-neutral-200" />
+          </div>
+          <div className="px-9 py-3 rounded-md bg-neutral-200 w-fit" />
+        </div>
+      </div>
+      <div className="shadow shadow-neutral-200 rounded-xl p-3">
+        <div className="animate-pulse space-y-7">
+          <div className="flex justify-between items-start">
+            <div className="rounded-md bg-neutral-200 px-14 py-2" />
+            <div className="rounded-md p-2.5 bg-neutral-200" />
+          </div>
+          <div className="px-9 py-3 rounded-md bg-neutral-200 w-fit" />
+        </div>
+      </div>
+    </div>
+  )
 }
