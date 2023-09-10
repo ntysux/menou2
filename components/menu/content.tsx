3d26af9c7e9194ba17b233077a05d341fc52afd1
Duong Quoc Viet <ntysux@gmail.com>
2023-09-10 18:48:56 +0700

    show empty if menu length = 0

--- components/menu/content.tsx
+++ components/menu/content.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import { initMenu } from '@/redux/menu/slice'
 import Card from './card'
 import CardDialog from './card.dialog'
+import { IconSoup } from '@tabler/icons-react'
 
 export default function MenuContent({pages}: {pages: any}) {
   const dispatch = useAppDispatch()
@@ -32,6 +33,24 @@ export default function MenuContent({pages}: {pages: any}) {
       }
     </div>
   ) : (
-    <>Trống</>
+    <Empty />
+  )
+}
+
+function Empty() {
+  return (
+    <div className="p-9 mt-3 space-y-5 text-center">
+      <div className="flex justify-center">
+        <IconSoup size='40px' className="text-neutral-200" />
+      </div>
+      <div>
+        <h2 className="text-lg text-neutral-600 font-light tracking-wider">
+          Món ăn được tạo sẽ hiển thị tại đây.
+        </h2>
+        <p className="text-sm text-neutral-400 font-light">
+          Nhấn vào <span className="text-neutral-600 font-medium">Món ăn mới</span> để thêm mới món ăn.
+        </p>
+      </div>
+    </div>
   )
 }
