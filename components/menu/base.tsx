aeeb3e21978db4e29bccb1aa262ae5f0ac141b8d
Duong Quoc Viet <ntysux@gmail.com>
2023-09-10 13:48:42 +0700

    using dashboard in base

--- components/menu/base.tsx
+++ components/menu/base.tsx
@@ -1,12 +1,15 @@
 import Nav from "../nav"
 import Sidebar from "../sidebar"
+import Dashboard from "./dashboard"
 
 export default function MenuBase({children}: {children: React.ReactNode}) {
   return (
     <div className="flex space-x-5 w-screen max-w-6xl mx-auto">
       <Sidebar />
       <div className="flex-1">
-        <Nav />
+        <Nav>
+          <Dashboard />
+        </Nav>
         {children}
       </div>
     </div>
