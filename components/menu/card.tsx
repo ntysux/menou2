3eabf0014498bee8d9d072aaef200e984a6c910a
Duong Quoc Viet <ntysux@gmail.com>
2023-09-10 14:43:08 +0700

    create & using: Card item

---
+++ components/menu/card.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { Menu } from '@/redux/menu/types'
+import Checkbox from '../checkbox'
+import { useAppDispatch } from '@/redux/hooks'
+import { checkedMenu } from '@/redux/menu/slice'
+
+interface Props {
+  page: Menu
+  index: number
+}
+
+export default function Card({page, index}: Props) {
+  const dispatch = useAppDispatch()
+
+  return (
+    <div className={`relative p-3 rounded-xl ${page.checked ? 'ring-2 ring-neutral-800' : 'shadow shadow-neutral-200 hover:ring-1 hover:ring-neutral-200'}`}>
+      <div className='space-y-9'>
+        <h3 className="truncate max-w-[150px] leading-3 text-xs font-bold text-neutral-800">
+          {page.name}
+        </h3>
+        <div className={`${page.status ? 'bg-purple-100 text-purple-800' : 'bg-sky-100 text-sky-800'} px-3 py-1 text-xs font-medium w-fit rounded-md`}>
+          {page.status ? 'Riêng tư' : 'Công khai'}
+        </div>
+      </div>
+
+      <div className='absolute top-3 right-3'>
+        <Checkbox 
+          checked={page.checked ?? false} 
+          onChange={e => dispatch(checkedMenu({checked: e.target.checked, index}))} 
+        />
+      </div>
+    </div>
+  )
+}
--- components/menu/content.tsx
+++ components/menu/content.tsx
@@ -2,8 +2,9 @@
 import Link from 'next/link'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { useEffect } from 'react'
-import { changeColorMultiMenu, changeColorSingleMenu, checkedMenu, checkedMultiMenu, initMenu, removeMenu, removeMultiMenu } from '@/redux/menu/slice'
+import { changeColorSingleMenu, checkedMenu, initMenu, removeMenu } from '@/redux/menu/slice'
 import CUDialog from './crud/c.u.dialog'
+import Card from './card'
 
 const url = process.env.NEXT_PUBLIC_APP_URL
 
@@ -15,14 +16,6 @@ async function handleDelete(id: string) {
   })
 }
 
-async function handleDeleteMulti(idList: string[]) {
-  await fetch(`${url}/menu/api/delete/multi`, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({idList})
-  })
-}
-
 async function handleChangeColorSingle(id: string, color: string) {
   await fetch(`${url}/menu/api/update/single/color`, {
     method: 'POST',
@@ -31,23 +24,9 @@ async function handleChangeColorSingle(id: string, color: string) {
   })
 }
 
-async function handleChangeColorMulti(idList: string[], color: string) {
-  await fetch(`${url}/menu/api/update/multi/color`, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({idList, color})
-  })
-}
-
 export default function MenuContent({pages}: {pages: any}) {
   const dispatch = useAppDispatch()
   const menu = useAppSelector(state => state.menu)
-  
-  const idList = menu.filter(menu => menu.checked).map(menu => menu.id)
-
-  const allChecked = menu.filter(item => !item.deleted).every(item => item.checked)
-  const isIndeterminate = menu.some(item => item.checked) && !allChecked
-
   useEffect(() => {
     if (!menu.length && pages.length) {
       dispatch(initMenu(pages))
@@ -55,91 +34,41 @@ export default function MenuContent({pages}: {pages: any}) {
   }, [])
 
   return (
-    <div className="w-screen max-w-5xl mx-auto">
-      <div className='flex space-x-3 items-center'>
-        <div className='relative bg-sky-200 p-3'>
+    <div className='mt-7 mb-20 grid grid-cols-1 gap-3 sm:mb-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+      {
+        menu.map((page: any, index: number) =>
+          !page.deleted &&
+          <Card key={index} page={page} index={index} />
+            
+            // <button onClick={() => {
+            //   dispatch(removeMenu(index))
+            //   handleDelete(page.id)
+            // }}>
+            //   Xóa
+            // </button>
+            // <CUDialog index={index}>
+            //   {setOpen => 
+            //     <button onClick={() => setOpen(true)}>
+            //       Sửa
+            //     </button>
+            //   }
+            // </CUDialog>
+            // <Link href={`/menu/${page.id}`}>
+            //   <button>Đầy đủ</button>
+            // </Link>
 
-          {isIndeterminate && <div className='absolute inset-0 p-1 px-3 rounded-full bg-neutral-500' />}
-          
-          <input 
-            checked={allChecked}
-            type="checkbox" 
-            className='w-5 h-5 accent-neutral-800 absolute z-10 inset-0'
-            onChange={e => dispatch(checkedMultiMenu(e.target.checked))}
-          />
-        </div>
-        <CUDialog>
-          {setOpen => 
-            <button onClick={() => setOpen(true)}>
-              Thêm mới
-            </button>
-          }
-        </CUDialog>
-        <button onClick={() => {
-          dispatch(removeMultiMenu())
-          handleDeleteMulti(idList)
-        }}>
-          Xóa
-        </button>
-        <div className='flex space-x-3'>
-          {
-            ['bg-rose-400', 'bg-teal-400', 'bg-purple-400', 'bg-white'].map((color, index) =>
-              <button key={index} onClick={() => {
-                dispatch(changeColorMultiMenu(color))
-                handleChangeColorMulti(idList, color)
-              }}>
-                <div className={`p-2 rounded-full ${color} ring-1 ring-neutral-800`} />  
-              </button>
-            )
-          }
-        </div>
-      </div>
-
-      <div className='mt-9'>
-        {
-          menu.map((page: any, index: number) =>
-            !page.deleted &&
-            <div key={index} className={`flex items-center space-x-3 ${page.color}`}>
-              <div>{index}</div>
-              <input 
-                type="checkbox" 
-                checked={page.checked ?? false}
-                className='w-5 h-5 accent-neutral-800'
-                onChange={e => dispatch(checkedMenu({checked: e.target.checked, index}))}
-              />
-              
-              <h2>{page.name}</h2>
-              <button onClick={() => {
-                dispatch(removeMenu(index))
-                handleDelete(page.id)
-              }}>
-                Xóa
-              </button>
-              <CUDialog index={index}>
-                {setOpen => 
-                  <button onClick={() => setOpen(true)}>
-                    Sửa
-                  </button>
-                }
-              </CUDialog>
-              <Link href={`/menu/${page.id}`}>
-                <button>Đầy đủ</button>
-              </Link>
-
-              {
-                ['bg-rose-400', 'bg-teal-400', 'bg-purple-400', 'bg-white'].map((color, colorIndex) =>
-                  <button key={colorIndex} onClick={() => {
-                    dispatch(changeColorSingleMenu({color, index}))
-                    handleChangeColorSingle(page.id, color)
-                  }}>
-                    <div className={`p-2 rounded-full ${color} ring-1 ring-neutral-800`} />  
-                  </button>
-                )
-              }
-            </div>
-          )
-        }
-      </div>
+            // {
+            //   ['bg-rose-400', 'bg-teal-400', 'bg-purple-400', 'bg-white'].map((color, colorIndex) =>
+            //     <button key={colorIndex} onClick={() => {
+            //       dispatch(changeColorSingleMenu({color, index}))
+            //       handleChangeColorSingle(page.id, color)
+            //     }}>
+            //       <div className={`p-2 rounded-full ${color} ring-1 ring-neutral-800`} />  
+            //     </button>
+            //   )
+            // }
+        )
+      }
     </div>
   )
 }
