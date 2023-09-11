e154b01f8cf950803db662b179e46f138602ebc0
Duong Quoc Viet <ntysux@gmail.com>
2023-09-11 17:40:44 +0700

    use Empty in page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,4 +1,5 @@
 import MenuContent from "@/components/menu/content"
+import { IconSoup } from "@tabler/icons-react"
 import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies"
 import { cookies } from "next/headers"
 
@@ -17,5 +18,29 @@ export default async function MenuPage() {
   const cookie = cookies().get('token')
   const rs = await allMenuPagesById(cookie)
 
-  return !rs.error ? <MenuContent pages={rs.menuPages} /> : <>{rs.error}</>
+  return rs.menuPages.length ? (
+    <MenuContent pages={rs.menuPages} />
+  ) : (
+    <>
+      {rs.error ?? <Empty />}
+    </>
+  )
+}
+
+function Empty() {
+  return (
+    <div className="p-9 mt-3 space-y-5 text-center">
+      <div className="flex justify-center">
+        <IconSoup size='40px' className="text-neutral-200" />
+      </div>
+      <div>
+        <h2 className="text-lg text-neutral-600 font-light tracking-wider">
+          Món ăn được tạo sẽ hiển thị tại đây.
+        </h2>
+        <p className="text-sm text-neutral-400 font-light">
+          Nhấn vào <span className="text-neutral-600 font-medium">Món ăn mới</span> để thêm mới món ăn.
+        </p>
+      </div>
+    </div>
+  )
 }
