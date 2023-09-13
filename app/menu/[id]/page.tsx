093683e889816d6987172a74e0305187933e95f8
Duong Quoc Viet <ntysux@gmail.com>
2023-09-13 16:30:41 +0700

    handle show Empty component

--- app/menu/[id]/page.tsx
+++ app/menu/[id]/page.tsx
@@ -17,7 +17,7 @@ export default async function ViewPage({params}: {params: {id: string}}) {
     result = await getPage(id)
 
   return result.page ? (
-    <div className="space-y-9">
+    <div className="space-y-9 mb-9 mx-3 sm:mx-0">
       <h2 className="text-lg text-neutral-800 font-bold">
         {result.page.name}
       </h2>
@@ -25,39 +25,66 @@ export default async function ViewPage({params}: {params: {id: string}}) {
         <h3 className="text-neutral-800 font-semibold">
           Hướng dẫn / chế biến
         </h3>
-        <h4 className="text-neutral-800 text-sm font-bold">
+        <h4 className="text-neutral-800 text-xs font-bold">
           Nguyên liệu
         </h4>
         <ul>
-          {result.page.materials?.split('|').map((item, index) =>
-            <li key={index} className="text-sm text-neutral-800 font-medium">
-              {item}
-            </li>
-          )}
+          {
+            result.page.materials?.length 
+            ?
+            result.page.materials?.split('|').map((item, index) =>
+              <li key={index} className="text-sm text-neutral-800 font-medium">
+                {item}
+              </li>
+            )
+            :
+            <Empty />
+          }
         </ul>
-        <h4 className="text-neutral-800 text-sm font-bold">
+        <h4 className="text-neutral-800 text-xs font-bold">
           Chuẩn bị
         </h4>
         <ul>
-          {result.page.required?.split('|').map((item, index) =>
-            <li key={index} className="text-sm text-neutral-800 font-medium">
-              {item}
-            </li>
-          )}
+          {
+            result.page.required?.length 
+            ?
+            result.page.required?.split('|').map((item, index) =>
+              <li key={index} className="text-sm text-neutral-800 font-medium">
+                {item}
+              </li>
+            )
+            :
+            <Empty />
+          }
         </ul>
-        <h4 className="text-neutral-800 text-sm font-bold">
+        <h4 className="text-neutral-800 text-xs font-bold">
           Chế biến
         </h4>
         <ul>
-          {result.page.steps?.split('|').map((item, index) =>
-            <li key={index} className="text-sm text-neutral-800 font-medium">
-              {item}
-            </li>
-          )}
+          {
+            result.page.steps?.length
+            ?
+            result.page.steps?.split('|').map((item, index) =>
+              <li key={index} className="text-sm text-neutral-800 font-medium">
+                {item}
+              </li>
+            )
+            :
+            <Empty />
+          }
         </ul>
       </div>
     </div>
   ) : (
     <>{result.error}</>
   )
+}
+
+function Empty() {
+  return (
+    <div className="p-9 border border-dashed border-neutral-300 rounded-lg text-center text-sm text-neutral-400 font-medium">
+      <p>Trống</p>
+      <p>Các bước/thành phần sẽ được hiển thị tại đây.</p>
+    </div>
+  ) 
 }
