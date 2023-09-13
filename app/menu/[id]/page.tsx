69456b4b9d6370715b1ccd1f0b186fb49f5c5f3a
Duong Quoc Viet <ntysux@gmail.com>
2023-09-13 15:43:02 +0700

    style ui

--- app/menu/[id]/page.tsx
+++ app/menu/[id]/page.tsx
@@ -1,49 +1,63 @@
+import { Menu } from "@/redux/menu/types"
+
+interface Result {
+  page?: Menu
+  error?: string
+}
 const url = process.env.NEXT_PUBLIC_APP_URL
 
 async function getPage(id: string) {
-  const res = await fetch(`${url}/menu/${id}/api`, {next: {revalidate: 0}})
-  const rs = await res.json()
-
-  return rs
+  const response = await fetch(`${url}/menu/${id}/api`, {next: {revalidate: 0}}),
+    result: Result = await response.json()
+  return result
 }
 
 export default async function ViewPage({params}: {params: {id: string}}) {
-  const {id} = params
-  const rs = await getPage(id)
+  const {id} = params,
+    result = await getPage(id)
 
-  return (
-    <>
-      <h2>{rs.name}</h2>
-      <h3 className="font-bold">Nguyên liệu</h3>
-      <ul>
-        {
-          rs.materials?.split('|').map((material: any, index: number) => 
-            <li key={index}>{material}</li> 
-          )
-          ??
-          <div>Trống</div>
-        }
-      </ul>
-      <h3 className="font-bold">Chuẩn bị</h3>
-      <ul>
-        {
-          rs.required?.split('|').map((require: any, index: number) => 
-            <li key={index}>{require}</li> 
-          )
-          ??
-          <div>Trống</div>
-        }
-      </ul>
-      <h3 className="font-bold">Chế biến</h3>
-      <ul>
-        {
-          rs.steps?.split('|').map((step: any, index: number) => 
-            <li key={index}>{step}</li> 
-          )
-          ??
-          <div>Trống</div>
-        }
-      </ul>
-    </>
+  return result.page ? (
+    <div className="space-y-9">
+      <h2 className="text-lg text-neutral-800 font-bold">
+        {result.page.name}
+      </h2>
+      <div className="space-y-3">
+        <h3 className="text-neutral-800 font-semibold">
+          Hướng dẫn / chế biến
+        </h3>
+        <h4 className="text-neutral-800 text-sm font-bold">
+          Nguyên liệu
+        </h4>
+        <ul>
+          {result.page.materials?.split('|').map((item, index) =>
+            <li key={index} className="text-sm text-neutral-800 font-medium">
+              {item}
+            </li>
+          )}
+        </ul>
+        <h4 className="text-neutral-800 text-sm font-bold">
+          Chuẩn bị
+        </h4>
+        <ul>
+          {result.page.required?.split('|').map((item, index) =>
+            <li key={index} className="text-sm text-neutral-800 font-medium">
+              {item}
+            </li>
+          )}
+        </ul>
+        <h4 className="text-neutral-800 text-sm font-bold">
+          Chế biến
+        </h4>
+        <ul>
+          {result.page.steps?.split('|').map((item, index) =>
+            <li key={index} className="text-sm text-neutral-800 font-medium">
+              {item}
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  ) : (
+    <>{result.error}</>
   )
 }
