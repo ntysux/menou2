710608ce86805fba48e43ac2cb1ea695d27e4494
Duong Quoc Viet <ntysux@gmail.com>
2023-09-13 15:18:54 +0700

    throws error if id dose not exist

--- app/menu/[id]/api/route.ts
+++ app/menu/[id]/api/route.ts
@@ -6,21 +6,24 @@ const notion = new Client({auth: process.env.NOTION_KEY})
 
 export async function GET(request: NextRequest, {params}: {params: {id: string}}) {
   const {id} = params
-  const pageResponse: any = await notion.pages.retrieve({
-    page_id: id
-  })
 
-  const {
-    properties: {
-      name, materials, required, steps, status
-    }
-  } = pageResponse
-
-  return NextResponse.json({
-    name: name.rich_text[0].plain_text,
-    materials: materials.rich_text[0]?.plain_text,
-    required: required.rich_text[0]?.plain_text,
-    steps: steps.rich_text[0]?.plain_text,
-    status: status.checkbox
-  })
+  try {
+    const pageResponse: any = await notion.pages.retrieve({
+      page_id: id
+    })
+    const {
+      properties: {name, materials, required, steps, status}
+    } = pageResponse
+    return NextResponse.json({
+      page: {
+        name: name.rich_text[0].plain_text,
+        materials: materials.rich_text[0]?.plain_text,
+        required: required.rich_text[0]?.plain_text,
+        steps: steps.rich_text[0]?.plain_text,
+        status: status.checkbox
+      }
+    })
+  } catch (error: any) {
+    return NextResponse.json({error: error.message})
+  }
 }
