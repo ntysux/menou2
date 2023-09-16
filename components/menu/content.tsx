225577e2ee9348a1bb453f252a55cbafad512ffb
Duong Quoc Viet <ntysux@gmail.com>
2023-09-16 09:11:09 +0700

    ui update & change logic

--- components/menu/content.tsx
+++ components/menu/content.tsx
@@ -2,11 +2,12 @@
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { useEffect } from 'react'
 import { init } from '@/redux/menu/slice'
+import { IconChefHat } from '@tabler/icons-react'
+import { Menu } from '@/redux/menu/types'
 import Card from './card'
 import CardDialog from './card.dialog'
-import { IconSoup } from '@tabler/icons-react'
 
-export default function MenuContent({pages}: {pages: any}) {
+export default function MenuContent({pages}: {pages: Menu[]}) {
   const dispatch = useAppDispatch()
   const menu = useAppSelector(state => state.menu)
   useEffect(() => {
@@ -15,7 +16,7 @@ export default function MenuContent({pages}: {pages: any}) {
     }
   }, [])
 
-  return menu.filter(page => !page.deleted).length ? (
+  return [menu, pages].some(array => array.filter(page => !page.deleted).length) ? (
     <div className='mt-7 gap-3 mx-3 mb-20 grid grid-cols-1 sm:mx-0 sm:mb-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {
         menu.map((page, index) =>
@@ -31,16 +32,16 @@ export default function MenuContent({pages}: {pages: any}) {
 
 function Empty() {
   return (
-    <div className="p-9 mt-3 space-y-5 text-center">
+    <div className="p-9 mt-5 mx-3 space-y-5 text-center border border-dashed border-neutral-300 rounded-lg sm:mx-0">
       <div className="flex justify-center">
-        <IconSoup size='40px' className="text-neutral-200" />
+        <IconChefHat size='35px' className="text-neutral-300" />
       </div>
       <div>
-        <h2 className="text-lg text-neutral-600 font-light tracking-wider">
-          Món ăn được tạo sẽ hiển thị tại đây.
+        <h2 className="text-sm text-neutral-400 font-medium tracking-wider">
+          Món ăn sẽ được hiển thị tại đây.
         </h2>
-        <p className="text-sm text-neutral-400 font-light">
-          Nhấn vào <span className="text-neutral-600 font-medium">Món ăn mới</span> để thêm mới món ăn.
+        <p className="text-xs text-neutral-400 font-medium">
+          Nhấn vào <span className="text-neutral-500 font-bold">Món ăn mới</span> để thêm mới món ăn.
         </p>
       </div>
     </div>
--- components/menu/trash/content.tsx
+++ components/menu/trash/content.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import { init } from '@/redux/menu/slice'
 import Card from '@/components/menu/card'
 import { Menu } from '@/redux/menu/types'
-import { IconSoup } from '@tabler/icons-react'
+import { IconTrashX } from '@tabler/icons-react'
 
 export default function MenuTrashContent({pages}: {pages: Menu[]}) {
   const dispatch = useAppDispatch()
@@ -15,7 +15,7 @@ export default function MenuTrashContent({pages}: {pages: Menu[]}) {
     }
   }, [])
 
-  return menu.filter(page => page.deleted).length ? (
+  return [menu, pages].some(array => array.filter(page => page.deleted).length) ? (
     <div className='mt-7 mb-20 grid grid-cols-1 gap-3 sm:mb-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {
         menu.map((page, index) =>
@@ -29,16 +29,16 @@ export default function MenuTrashContent({pages}: {pages: Menu[]}) {
 
 function Empty() {
   return (
-    <div className="p-9 mt-3 space-y-5 text-center">
+    <div className="p-9 mt-5 mx-3 space-y-5 text-center border border-dashed border-neutral-300 rounded-lg sm:mx-0">
       <div className="flex justify-center">
-        <IconSoup size='40px' className="text-neutral-200" />
+        <IconTrashX size='35px' className="text-neutral-300" />
       </div>
       <div>
-        <h2 className="text-lg text-neutral-600 font-light tracking-wider">
-          Món ăn đã xóa sẽ hiển thị tại đây.
+        <h2 className="text-sm text-neutral-400 font-medium tracking-wider">
+          Món ăn đã xóa sẽ được hiển thị tại đây.
         </h2>
-        <p className="text-sm text-neutral-400 font-light">
-          Nhấn vào <span className="text-neutral-600 font-medium">Khôi phục</span> để khôi phục món ăn.
+        <p className="text-xs text-neutral-400 font-medium">
+          Nhấn vào <span className="text-neutral-500 font-bold">Món ăn mới</span> để thêm mới món ăn.
         </p>
       </div>
     </div>
