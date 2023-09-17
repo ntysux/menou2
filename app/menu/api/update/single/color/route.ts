0f52d1f87e39ca0494e9199fd92ddf40f545f9c8
Duong Quoc Viet <ntysux@gmail.com>
2023-09-17 07:12:28 +0700

    return id page after updated

--- app/menu/api/update/single/color/route.ts
+++ app/menu/api/update/single/color/route.ts
@@ -5,20 +5,20 @@ import { Client } from "@notionhq/client"
 const notion = new Client({auth: process.env.NOTION_KEY})
 
 export async function POST(request: NextRequest) {
-  const {id, color}: {id: string, color: string} = await request.json()
+  const {id: pageId, color}: {id: string, color: string} = await request.json()
 
-    await notion.pages.update({
-      page_id: id,
-      properties: {
-        color: {
-          rich_text: [{
-            text: {
-              content: color
-            }
-          }]
-        }
+  const {id} = await notion.pages.update({
+    page_id: pageId,
+    properties: {
+      color: {
+        rich_text: [{
+          text: {
+            content: color
+          }
+        }]
       }
-    })
+    }
+  })
   
-  return NextResponse.json({})
+  return NextResponse.json({id})
 }
