1a8015ed7861ff7b63dd9c2e9959ebbcdaee881d
Duong Quoc Viet <ntysux@gmail.com>
2023-09-20 09:50:23 +0700

    create & using: dialog content

---
+++ components/menu/card/dialog/content.tsx
@@ -0,0 +1,53 @@
+import Checkbox from "@/components/checkbox"
+import { useAppSelector } from "@/redux/hooks"
+import { Tab } from "@headlessui/react"
+import { motion } from "framer-motion"
+import Empty from "./empty"
+
+const transition = {
+  initial: { y: 10, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  exit: { y: -10, opacity: 0 },
+  transition: { duration: 0.2 }
+}
+
+export default function Content({index}: {index: number}) {
+  const {materials, required, steps} = useAppSelector(state => state.menu)[index]
+  const panels = [materials?.split('|'), required?.split('|'), steps?.split('|')]
+
+  return (
+    <Tab.Panels className="px-6">
+      {panels.map((panel, panelIndex) => (
+        <Tab.Panel key={panelIndex}>
+          {
+            panel 
+            ?
+            <motion.div {...transition}>
+              <ul>
+                {panel?.map((line, lineIndex) => (
+                  <li key={lineIndex} className="p-1 text-neutral-800 text-sm font-medium">
+                    {
+                      !panelIndex 
+                      ?
+                      <Checkbox>
+                        {isChecked => 
+                          <span className={`${isChecked && 'line-through text-neutral-400/75'}`}>
+                            {line}
+                          </span>
+                        }
+                      </Checkbox>
+                      :
+                      line
+                    }
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+            :
+            <Empty />
+          }
+        </Tab.Panel>
+      ))}
+    </Tab.Panels>
+  )
+}
