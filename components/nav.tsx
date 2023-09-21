ede44984b4dda1ef38a9a60b60ac586e90130303
Duong Quoc Viet <ntysux@gmail.com>
2023-09-22 00:09:31 +0700

    chore: add padding

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,8 +6,8 @@ export default function Nav({children}: {children: React.ReactNode}) {
   const pathname = usePathname()
 
   return (
-    <header className="sticky top-0 p-3 space-y-3 bg-white">
-      <nav className="flex items-center justify-between">
+    <header className="sticky top-0 z-10 bg-white px-3 sm:px-0">
+      <nav className="flex items-center justify-between py-3">
         <h1 className='text-xl text-neutral-800 tracking-widest sm:opacity-0'>
           Menoú
         </h1>
