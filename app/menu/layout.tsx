a2fb8c722d490b79c106b8727cb172100ec8cde5
Duong Quoc Viet <ntysux@gmail.com>
2023-09-22 10:57:03 +0700

    chore: remove m-y

--- app/menu/layout.tsx
+++ app/menu/layout.tsx
@@ -7,9 +7,7 @@ export default function MenuLayout({children}: {children: React.ReactNode}) {
   return (
     <>
       {!cookie && <SigninDialog />}
-      <div className="my-3">
-        {children}
-      </div>
+      {children}
     </>
   )
 }
