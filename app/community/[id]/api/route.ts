e8ebfee68d3cca620ce368f38b09e3c245acb80f
Duong Quoc Viet <ntysux@gmail.com>
2023-09-24 01:23:59 +0700

    create api: get one menu page public by id

---
+++ app/community/[id]/api/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
+import { Client } from "@notionhq/client"
+
+const notion = new Client({auth: process.env.NOTION_KEY})
+
+export async function GET(request: NextRequest, {params}: {params: {id: string}}) {
+  const {id} = params
+
+  try {
+    const {properties: {uid, name, materials, required, steps}}: any = await notion.pages.retrieve({
+      page_id: id
+    })
+
+    const {
+      properties: {
+        name: {rich_text: [{plain_text}]},
+        verified: {checkbox}
+      }
+    }: any = await notion.pages.retrieve({
+      page_id: uid.title[0].plain_text
+    }) 
+
+    return NextResponse.json({
+      page: {
+        uid: uid.title[0].plain_text,
+        name: name.rich_text[0].plain_text,
+        materials: materials.rich_text[0]?.plain_text,
+        required: required.rich_text[0]?.plain_text,
+        steps: steps.rich_text[0]?.plain_text,
+        uname: plain_text,
+        verified: checkbox
+      }
+    })
+  } catch (error: any) {
+    return NextResponse.json({error: {code: error.code, message: error.message}})
+  }
+}
