a9334e2b5f5c1dc356b918a4c0d21efc676968f4
Duong Quoc Viet <ntysux@gmail.com>
2023-09-23 14:40:05 +0700

    handle show search page if search query = true

--- components/community/content.tsx
+++ components/community/content.tsx
@@ -4,8 +4,10 @@ import { init } from "@/redux/menu.public/slice"
 import { MenuPublic } from "@/redux/menu.public/types"
 import { useEffect } from "react"
 import Card from "./card/card"
+import { useSearchParams } from "next/navigation"
 
 export default function Content({pages}: {pages: MenuPublic[]}) {
+  const search = useSearchParams().get('search')
   const menuPublic = useAppSelector(state => state.menuPublic)
   const dispatch = useAppDispatch()
   useEffect(() => {
@@ -14,7 +16,18 @@ export default function Content({pages}: {pages: MenuPublic[]}) {
     }
   }, [])
 
-  return (
+  return search ? (
+    <div className="space-y-3">
+      <h2 className="text-sm text-neutral-800 font-bold">
+        {menuPublic.filter(page => page.name.toLowerCase().includes(search.toLowerCase())).length} kết quả
+      </h2>
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+        {menuPublic.map((page, index) => 
+          page.name.toLowerCase().includes(search.toLowerCase()) && <Card key={index} page={page} />
+        )}
+      </div>
+    </div>
+  ) : (
     <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
       {menuPublic.map((page, index) => 
         <Card key={index} page={page} />
