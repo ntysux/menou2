80bfe346dd4e462298819592ab6c63c33bc21680
Duong Quoc Viet <ntysux@gmail.com>
2023-09-23 12:36:48 +0700

    stop using children & handle logic into comp

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,7 +1,12 @@
 'use client'
+import { usePathname } from "next/navigation"
 import RouterMobile from "./router.mobile"
+import Dashboard from "./menu/dashboard/dashboard"
+import Search from "./community/search"
 
-export default function Nav({children}: {children: React.ReactNode}) {
+export default function Nav() {
+  const pathname = usePathname()
+
   return (
     <header className="sticky top-0 z-10 bg-white px-3 sm:px-0">
       <nav className="flex items-center justify-between py-3">
@@ -12,7 +17,8 @@ export default function Nav({children}: {children: React.ReactNode}) {
           <RouterMobile />
         </div>
       </nav>
-      {children}
+      {['/menu', '/menu/trash'].some(url => url === pathname) && <Dashboard />}
+      {pathname.startsWith('/community') && <Search />}
     </header>
   )
 }
