0e064ef0be91a1213a9b23a3e1b98901fb496894
Duong Quoc Viet <ntysux@gmail.com>
2023-09-23 23:54:42 +0700

    create & using: community loading

---
+++ app/community/loading.tsx
@@ -0,0 +1,34 @@
+export default function Loading() {
+  return (
+    <div className="grid grid-cols-2 gap-3">
+      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+        <div className="space-y-1">
+          <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
+          <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
+        </div>
+        <div className="p-2 w-2/3 bg-neutral-200 rounded-sm" />
+      </div>
+      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+        <div className="space-y-1">
+          <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
+          <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
+        </div>
+        <div className="p-2 w-2/3 bg-neutral-200 rounded-sm" />
+      </div>
+      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+        <div className="space-y-1">
+          <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
+          <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
+        </div>
+        <div className="p-2 w-2/3 bg-neutral-200 rounded-sm" />
+      </div>
+      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+        <div className="space-y-1">
+          <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
+          <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
+        </div>
+        <div className="p-2 w-2/3 bg-neutral-200 rounded-sm" />
+      </div>
+    </div>
+  )
+}
