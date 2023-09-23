76d9c1dd27757bead218d7ee4f01068dd8b9bd38
Duong Quoc Viet <ntysux@gmail.com>
2023-09-23 09:53:59 +0700

    add hero-pattern theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'hero-pattern': `url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%2309090b' fill-opacity='0.75' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");`,
       },
       boxShadow: {
         'custombox': '-10px -10px 15px rgba(255,255,255,0.5),10px 10px 15px rgba(70,70,70,0.12);'
