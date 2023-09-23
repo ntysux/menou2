7e29e1d05196412bd06b5467abc7281b79f5353e
Duong Quoc Viet <ntysux@gmail.com>
2023-09-23 12:56:06 +0700

    chore: update ui

--- app/menu/layout.tsx
+++ app/menu/layout.tsx
@@ -1,4 +1,6 @@
 import SigninDialog from "@/components/auth/signin/signin.dialog"
+import Nav from "@/components/nav"
+import Sidebar from "@/components/sidebar/sidebar"
 import { cookies } from "next/headers"
 
 export default function MenuLayout({children}: {children: React.ReactNode}) {
@@ -7,7 +9,24 @@ export default function MenuLayout({children}: {children: React.ReactNode}) {
   return (
     <>
       {!cookie && <SigninDialog />}
-      {children}
+      <div className='sm:flex sm:gap-3 sm:w-screen sm:max-w-5xl sm:mx-auto'>
+        <div className='hidden sm:block'>
+          <div className='sticky top-0'>
+            <div className='py-3'>
+              <h1 className='text-xl text-neutral-800 tracking-widest block'>
+                Menoú
+              </h1>
+            </div>
+            <Sidebar />
+          </div>
+        </div>
+        <div className='sm:flex-1'>
+          <Nav />
+          <div className='m-3 sm:mx-0'>
+            {children}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
