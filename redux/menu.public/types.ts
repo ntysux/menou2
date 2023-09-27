f96e6f4f74c79dee2722ff2b87436ca5ef6d9a07
Duong Quoc Viet <ntysux@gmail.com>
2023-09-27 09:03:54 +0700

    add & export type Comment

--- redux/menu.public/types.ts
+++ redux/menu.public/types.ts
@@ -1,6 +1,16 @@
 import { Menu } from "../menu/types"
 
+export interface Comment {
+  user: {
+    id: string,
+    name: string
+    verified: boolean 
+  }
+  comment: string
+}
+
 export type MenuPublic = Omit<Menu, 'deleted' | 'status' | 'checked' | 'color'> & {
   uname: string 
   verified: boolean
+  comment: Comment[]
 }
