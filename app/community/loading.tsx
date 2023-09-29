ef7b742b092b50b65c9cb60af8c7ec4e47ed8a36
Duong Quoc Viet <ntysux@gmail.com>
2023-09-29 20:52:06 +0700

    chore: col-2 --> col-1

--- app/community/loading.tsx
+++ app/community/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="grid grid-cols-2 gap-3">
+    <div className="grid grid-cols-1 gap-3">
       <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
         <div className="space-y-1">
           <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
