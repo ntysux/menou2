62b942d2e9cfc81a83cc8b7346a3574f23d8c480
Duong Quoc Viet <ntysux@gmail.com>
2023-10-03 21:12:51 +0700

    chore: update ui

--- app/community/loading.tsx
+++ app/community/loading.tsx
@@ -1,28 +1,28 @@
 export default function Loading() {
   return (
-    <div className="grid grid-cols-1 gap-3">
-      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+    <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+      <div className="p-9 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
         <div className="space-y-1">
           <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
           <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
         </div>
         <div className="p-2 w-2/3 bg-neutral-200 rounded-sm" />
       </div>
-      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+      <div className="p-9 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
         <div className="space-y-1">
           <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
           <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
         </div>
         <div className="p-2 w-2/3 bg-neutral-200 rounded-sm" />
       </div>
-      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+      <div className="p-9 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
         <div className="space-y-1">
           <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
           <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
         </div>
         <div className="p-2 w-2/3 bg-neutral-200 rounded-sm" />
       </div>
-      <div className="px-7 py-3 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
+      <div className="p-9 space-y-3 rounded-xl shadow shadow-neutral-200 animate-pulse">
         <div className="space-y-1">
           <div className="p-2.5 w-1/2 bg-neutral-200 rounded-sm" />
           <div className="p-2 w-1/3 bg-neutral-200 rounded-sm" />
