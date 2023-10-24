1c63e780dc1e951a5c99acec687f5a196938b9c7
Duong Quoc Viet <ntysux@gmail.com>
2023-10-24 10:43:02 +0700

    cache = no-store

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -3,7 +3,7 @@ import { MenuPublicPreview } from "@/redux/menu.public/types"
 import { url } from "@/utils/app.url"
 
 async function getAllPublicMenu(): Promise<MenuPublicPreview[]> {
-  const response = await fetch(`${url}/community/api`, {next: {revalidate: 0}})
+  const response = await fetch(`${url}/community/api`, {cache: 'no-store'})
   const result: {pages: MenuPublicPreview[]} = await response.json()
   return result.pages
 }
