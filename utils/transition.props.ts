6bbd899c2239c06fae34c6c25f9692bb5049693c
Duong Quoc Viet <ntysux@gmail.com>
2023-10-24 12:28:05 +0700

    create: props for transition

---
+++ utils/transition.props.ts
@@ -0,0 +1,26 @@
+export const overlay = {
+  enter:"ease-out duration-300",
+  enterFrom:"opacity-0",
+  enterTo:"opacity-100",
+  leave:"ease-in duration-200",
+  leaveFrom:"opacity-100",
+  leaveTo:"opacity-0"
+}
+
+export const scale = {
+  enter: "ease-out duration-300",
+  enterFrom: "opacity-0 scale-95",
+  enterTo: "opacity-100 scale-100",
+  leave: "ease-in duration-200",
+  leaveFrom: "opacity-100 scale-100",
+  leaveTo: "opacity-0 scale-95"
+}
+
+export const translateY = {
+  enter: "transition ease-out duration-200",
+  enterFrom: "opacity-0 translate-y-1",
+  enterTo: "opacity-100 translate-y-0",
+  leave: "transition ease-in duration-150",
+  leaveFrom: "opacity-100 translate-y-0",
+  leaveTo: "opacity-0 translate-y-1"
+}
