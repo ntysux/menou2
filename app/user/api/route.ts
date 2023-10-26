4a60295d9eea0d4054ade517488163a3cd3b6718
Duong Quoc Viet <ntysux@gmail.com>
2023-10-26 23:08:41 +0700

    add type User & change error text render

--- app/user/api/route.ts
+++ app/user/api/route.ts
@@ -3,6 +3,7 @@ import { type NextRequest } from 'next/server'
 import { Client } from "@notionhq/client"
 import { JwtPayload, decode } from 'jsonwebtoken'
 import { cookies } from 'next/headers'
+import { User } from '@/redux/user/types'
 
 const notion = new Client({auth: process.env.NOTION_KEY})
 
@@ -12,23 +13,20 @@ export async function GET(request: NextRequest) {
   if (cookie) {
     const {value: token} = cookie
     const tokenDecoded: string | JwtPayload | null = decode(token)
-
     if (typeof tokenDecoded !== 'string' && tokenDecoded) {
-      const uid: string = tokenDecoded['id']
-      
-      const {properties: {name, verified}}: any = await notion.pages.retrieve({
-        page_id: uid
-      })
+      const 
+        uid: string = tokenDecoded['id'],
+        {properties: {name, verified}}: any = await notion.pages.retrieve({page_id: uid})
 
-      return NextResponse.json({
-        user: {
-          id: uid,
-          name: name.rich_text[0].plain_text, 
-          verified: verified.checkbox
-        }
-      })
+      const user: User = {
+        id: uid,
+        name: name.rich_text[0].plain_text, 
+        verified: verified.checkbox
+      }
+
+      return NextResponse.json(user)
     }
   }
 
-  return NextResponse.json({error: 'Lỗi: unknown_user'})
+  return NextResponse.json({error: 'Lỗi: người dùng không xác định !'})
 }
