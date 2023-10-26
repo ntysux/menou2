832ca32d7beae0c491c906d207624114fac3f10e
Duong Quoc Viet <ntysux@gmail.com>
2023-10-27 00:09:19 +0700

    render user replace with {user}

--- app/user/api/route.ts
+++ app/user/api/route.ts
@@ -24,7 +24,7 @@ export async function GET(request: NextRequest) {
         verified: verified.checkbox
       }
 
-      return NextResponse.json(user)
+      return NextResponse.json({user})
     }
   }
 
