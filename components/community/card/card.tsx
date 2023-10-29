5a33c9fbd430a7621f2fa4cc993dd65f8176e385
Duong Quoc Viet <ntysux@gmail.com>
2023-10-30 06:56:51 +0700

    add Link, pass index for get page in state & remove OnClick

--- components/community/card/card.tsx
+++ components/community/card/card.tsx
@@ -1,25 +1,25 @@
 'use client'
-import { MenuPublic } from "@/redux/menu.public/types"
+import { useAppSelector } from "@/redux/hooks"
 import { IconDiscountCheckFilled } from "@tabler/icons-react"
+import Link from "next/link"
 
 interface Props {
-  page: MenuPublic
-  onClick: () => void
+  index: number
 }
 
-export default function Card({page, onClick}: Props) {
-  const {name, author: {name: authorName, verified}} = page
+export default function Card({index}: Props) {
+  const page = useAppSelector(state => state.menuPublic)[index]
 
   return (
-    <div onClick={onClick}>
+    <Link href={`/community/${page.id}`}>
       <div className="p-3 pb-11 bg-hero-pattern rounded-lg">
         <div className="flex items-end space-x-3 bg-white w-fit">
           <i className="text-xs text-neutral-600 font-medium">bởi</i>
           <div className="flex items-center space-x-1">
             <span className="text-sm text-neutral-800 font-bold truncate max-w-[100px]">
-              {authorName}
+              {page?.author.name}
             </span>
-            {verified && <IconDiscountCheckFilled size='17px' className="text-cyan-400" />}
+            {page?.author.verified && <IconDiscountCheckFilled size='17px' className="text-cyan-400" />}
           </div>
         </div>
         <i className="text-xs text-neutral-400 font-medium bg-white">
@@ -28,9 +28,9 @@ export default function Card({page, onClick}: Props) {
       </div>
       <div className="mt-1">
         <h2 className="text-sm text-neutral-800 font-bold truncate max-w-[150px]">
-          {name}
+          {page?.name}
         </h2>
       </div>
-    </div>
+    </Link>
   )
 }
