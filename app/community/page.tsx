2506d360925edd9e11ff4401ef5d6763af6397b5
Duong Quoc Viet <ntysux@gmail.com>
2023-10-30 09:49:22 +0700

    chore: change MenuPublic -> MenuPublicPreview

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,15 +1,14 @@
 import Content from "@/components/community/content"
-import { MenuPublic } from "@/redux/menu.public/types"
+import { MenuPublicPreview } from "@/redux/menu.public/types"
 import { url } from "@/utils/app.url"
 
-async function getAllMenuPublic(): Promise<MenuPublic[]> {
+async function getAllMenuPublic(): Promise<MenuPublicPreview[]> {
   const response = await fetch(`${url}/community/api`, {cache: 'no-store'})
   const result = await response.json()
-  return result.menuPublic
+  return result.menuPublicPreview
 }
 
-export default async function CommunityPage() {
-  const menuPublic = await getAllMenuPublic()
-  
-  return <Content pages={menuPublic} />
+export default async function Page() {
+  const pages = await getAllMenuPublic()
+  return <Content pages={pages} />
 }
