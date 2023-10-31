3c938b4d67abe2d165c56902f63391fcbf0c0d5e
Duong Quoc Viet <ntysux@gmail.com>
2023-10-31 13:53:52 +0700

    update Empty fc

--- components/empty.tsx
+++ components/empty.tsx
@@ -1,7 +1,19 @@
-export default function Empty({children}: {children: React.ReactNode}) {
+interface Props {
+  children: string | React.ReactNode
+}
+
+export default function Empty({children}: Props) {
   return (
-    <div className="p-9 border border-dashed border-neutral-400 rounded-lg text-center text-sm text-neutral-400 font-medium">
-      {children}
+    <div className="p-9 border border-dashed border-neutral-400 rounded-lg flex items-center justify-center">
+      {
+        typeof children === 'string'
+        ? 
+        <p className="text-sm text-neutral-500 font-medium">
+          {children}
+        </p>
+        :
+        <>{children}</> 
+      }
     </div>
   )
 }
