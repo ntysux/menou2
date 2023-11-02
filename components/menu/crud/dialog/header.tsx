62949190b0833cd19f303e624c8363024a5a4107
Duong Quoc Viet <ntysux@gmail.com>
2023-11-03 00:28:55 +0700

    chore: style header update

--- components/menu/crud/dialog/header.tsx
+++ components/menu/crud/dialog/header.tsx
@@ -1,15 +1,15 @@
-import { IconChevronRight } from "@tabler/icons-react"
+import { IconChevronsRight } from "@tabler/icons-react"
 import { OpenDialog } from "../types/types"
 import Tip from "../tip"
 
 export default function Header({setOpen}: {setOpen: OpenDialog}) {
   return (
-    <div className='sticky top-0 z-20 flex p-1 border-b border-neutral-200'>
+    <div className='sticky top-0 z-20 p-1 flex border-b border-neutral-200'>
       <button 
-        className='outline-none p-1.5 text-neutral-300 hover:text-neutral-500'
+        className='outline-none rounded-md p-1 text-neutral-300 hover:text-neutral-400'
         onClick={() => setOpen(false)}
       >
-        <IconChevronRight size='20px' strokeWidth='3' />
+        <IconChevronsRight size='20px' />
       </button>
       <Tip />
     </div>
--- components/menu/crud/tip.tsx
+++ components/menu/crud/tip.tsx
@@ -16,8 +16,8 @@ export default function Tip() {
     <Popover className="relative flex z-10">
       {({ open }) => (
         <>
-          <Popover.Button className={`${open ? 'text-neutral-500' : 'text-neutral-300'} outline-none p-1.5 hover:text-neutral-500 rounded-lg`}>
-            <IconBulb size='20px' strokeWidth='2' />
+          <Popover.Button className={`${open ? 'text-neutral-500' : 'text-neutral-300'} outline-none p-1 rounded-md hover:text-neutral-400`}>
+            <IconBulb size='20px' />
           </Popover.Button>
           <Transition as={Fragment} {...translateY}>
             <Popover.Panel className='absolute z-10 left-0 -bottom-7 min-w-max p-1 px-2 rounded-sm text-xs text-white font-bold bg-neutral-950/75'>
