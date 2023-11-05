0ab9fd5630258271a8c2954a6519f826b68d5b68
Duong Quoc Viet <ntysux@gmail.com>
2023-11-05 10:46:58 +0700

    using Empty & change cols

--- components/trash/content.tsx
+++ components/trash/content.tsx
@@ -5,6 +5,7 @@ import { init } from '@/redux/menu/slice'
 import Card from '@/components/menu/card/card'
 import { Menu } from '@/redux/menu/types'
 import { IconTrashX } from '@tabler/icons-react'
+import Empty from '../empty'
 
 export default function MenuTrashContent({pages}: {pages: Menu[]}) {
   const dispatch = useAppDispatch()
@@ -16,7 +17,7 @@ export default function MenuTrashContent({pages}: {pages: Menu[]}) {
   }, [])
 
   return [menu, pages].some(array => array.filter(page => page.deleted).length) ? (
-    <div className='mt-7 mb-20 mx-3 grid grid-cols-1 gap-3 sm:mb-0 sm:mx-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+    <div className='grid grid-cols-1 gap-3 xl:grid-cols-5 lg:grid-cols-3 md:grid-cols-2'>
       {
         menu.map((page, index) =>
           page.deleted &&
@@ -24,23 +25,17 @@ export default function MenuTrashContent({pages}: {pages: Menu[]}) {
         )
       }
     </div>
-  ) : <Empty />
-}
-
-function Empty() {
-  return (
-    <div className="p-9 space-y-5 text-center border border-dashed border-neutral-300 rounded-lg">
-      <div className="flex justify-center">
+  ) : (
+    <Empty>
+      <Empty.Icon>
         <IconTrashX size='35px' className="text-neutral-300" />
-      </div>
-      <div>
-        <h2 className="text-sm text-neutral-400 font-medium tracking-wider">
-          Món ăn đã xóa sẽ được hiển thị tại đây.
-        </h2>
-        <p className="text-xs text-neutral-400 font-medium">
-          Nhấn vào <span className="text-neutral-500 font-bold">Món ăn mới</span> để thêm mới món ăn.
-        </p>
-      </div>
-    </div>
+      </Empty.Icon>
+      <Empty.Text className='mt-3 text-sm text-neutral-500 font-medium'>
+        Món ăn đã xóa sẽ được hiển thị tại đây.
+      </Empty.Text>
+      <Empty.Text className='text-sm text-neutral-400'>
+        Nhấn vào <span className="text-neutral-500 font-bold">Mới</span> để thêm mới món ăn.
+      </Empty.Text>
+    </Empty>
   )
 }
