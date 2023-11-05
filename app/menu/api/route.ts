780eb295066568dc617e08af49823d3802724f86
Duong Quoc Viet <ntysux@gmail.com>
2023-11-06 00:21:13 +0700

    return: pages + user

--- app/menu/api/route.ts
+++ app/menu/api/route.ts
@@ -3,9 +3,50 @@ import { type NextRequest } from 'next/server'
 import { Client } from "@notionhq/client"
 import { JwtPayload, decode } from 'jsonwebtoken'
 import { cookies } from 'next/headers'
+import { User } from '@/redux/user/types'
+import { Menu } from '@/redux/menu/types'
 
 const notion = new Client({auth: process.env.NOTION_KEY})
 const notionMenouId = process.env.NOTION_MENOU
+
+async function renderUser(uid: string): Promise<User> {
+  const {properties: {name, verified, premium}}: any = await notion.pages.retrieve({
+    page_id: uid
+  })
+  return {
+    id: uid,
+    name: name.rich_text[0].plain_text, 
+    verified: verified.checkbox,
+    premium: premium.checkbox
+  }
+}
+
+async function renderMenu(uid: string): Promise<Menu[]> {
+  const {results} = await notion.databases.query({
+    database_id: notionMenouId!,
+    filter: {
+      property: 'uid',
+      rich_text: {
+        contains: uid
+      }
+    } 
+  })
+  return [...results.map((page: any) => {
+    const {id, properties: {uid, name, description, deleted, materials, required, steps, status, color}} = page
+    return {
+      id,
+      uid: uid.title[0].plain_text,
+      name: name.rich_text[0].plain_text,
+      description: description.rich_text[0]?.plain_text,
+      deleted: deleted.checkbox,
+      materials: materials.rich_text[0]?.plain_text,
+      required: required.rich_text[0]?.plain_text,
+      steps: steps.rich_text[0]?.plain_text,
+      status: status.checkbox,
+      color: color.rich_text[0]?.plain_text
+    }
+  })]
+}
 
 export async function GET(request: NextRequest) {
   const cookie = cookies().get('token')
@@ -13,40 +54,12 @@ export async function GET(request: NextRequest) {
   if (cookie) {
     const {value: token} = cookie
     const tokenDecoded: string | JwtPayload | null = decode(token)
-
     if (typeof tokenDecoded !== 'string' && tokenDecoded) {
       const uid: string = tokenDecoded['id']
-      
-      const {results} = await notion.databases.query({
-        database_id: notionMenouId!,
-        filter: {
-          property: 'uid',
-          rich_text: {
-            contains: uid
-          }
-        } 
-      })
+      const user = await renderUser(uid)      
+      const pages = await renderMenu(uid)
 
-      const pages = [...results.map((page: any) => {
-        const {id, properties: {
-          uid, name, description, deleted, materials, required, steps, status, color
-        }} = page
-  
-        return {
-          id,
-          uid: uid.title[0].plain_text,
-          name: name.rich_text[0].plain_text,
-          description: description.rich_text[0]?.plain_text,
-          deleted: deleted.checkbox,
-          materials: materials.rich_text[0]?.plain_text,
-          required: required.rich_text[0]?.plain_text,
-          steps: steps.rich_text[0]?.plain_text,
-          status: status.checkbox,
-          color: color.rich_text[0]?.plain_text
-        }
-      })]
-
-      return NextResponse.json({pages})
+      return NextResponse.json({results: {pages, user}})
     }
   }
 
