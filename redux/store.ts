b876d245227d54b4222e92463416638ba22c9e74
Duong Quoc Viet <ntysux@gmail.com>
2023-11-10 02:16:29 +0700

    add communitySettings reducer in store

--- redux/store.ts
+++ redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import { menuSlice } from './menu/slice'
 import { menuPublicSlice } from './menu.public/slice'
 import { userSlice } from './user/slice'
+import { communitySettingsSlice } from './community.settings/slice'
 
 export const store = configureStore({
   reducer: {
     menu: menuSlice.reducer,
     menuPublic: menuPublicSlice.reducer,
-    user: userSlice.reducer
+    user: userSlice.reducer,
+    communitySettings: communitySettingsSlice.reducer
   }
 })
 
