8f25f2223943e59a06c610e57e0a721e92f05834
Duong Quoc Viet <ntysux@gmail.com>
2023-11-10 13:27:52 +0700

    handle show Card Grid & List based on layout & set layout init if null

--- components/community/content.tsx
+++ components/community/content.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
-import { init } from "@/redux/menu.public/slice"
+import { init as menuPublicInit } from "@/redux/menu.public/slice"
+import { init as communitySettingsInit } from "@/redux/community.settings/slice"
 import { useEffect } from "react"
 import { useSearchParams } from "next/navigation"
 import { MenuPublicPreview } from "@/redux/menu.public/types"
@@ -14,11 +15,15 @@ export default function Content({pages}: Props) {
   const 
     dispatch = useAppDispatch(),
     menuPublic = useAppSelector(state => state.menuPublic),
+    {layout} = useAppSelector(state => state.communitySettings),
     search = useSearchParams().get('search')
 
   useEffect(() => {
-    if(!menuPublic.length && pages.length) {
-      dispatch(init(pages))
+    if (!menuPublic.length && pages.length) {
+      dispatch(menuPublicInit(pages))
+    }
+    if (layout === null) {
+      dispatch(communitySettingsInit({layout: false, display: null}))
     }
   }, [])
 
@@ -30,19 +35,28 @@ export default function Content({pages}: Props) {
           {menuPublic.filter(page => page.name.toLowerCase().includes(search.toLowerCase())).length} kết quả cho {search}
         </h2>
       }
-      <div className="grid grid-cols-1 sm:grid-cols-5 gap-x-3 gap-y-7">
+      <div className={`grid grid-cols-1 ${!layout && 'sm:grid-cols-5'} gap-x-3 gap-y-5`}>
         {
           search && 
           menuPublic.map((page, index) =>
             page.name.toLowerCase().includes(search.toLowerCase())
-            && 
-            <Card key={index} index={index} />
+            && (
+              !layout
+              ?
+              <Card.Grid key={index} index={index} />
+              :
+              <Card.List key={index} index={index} />
+            )
           )
         }
         {
           !search && 
           menuPublic.map((page, index) =>
-            <Card key={index} index={index} />
+            !layout 
+            ?
+            <Card.Grid key={index} index={index} />
+            :
+            <Card.List key={index} index={index} />
           )
         }
       </div>
