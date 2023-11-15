a1e4a45be301405f36227bf0207f3456fbb99786
Duong Quoc Viet <ntysux@gmail.com>
2023-11-15 23:12:26 +0700

    create fc: Editable

---
+++ components/editable/editable.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, Children, Dispatch, ReactElement, SetStateAction, cloneElement, isValidElement, useEffect, useRef, useState } from "react"
+
+interface EditableProps {
+  children: React.ReactNode
+  value: string
+  className?: string
+}
+
+interface EditableChildrenProps {
+  value?: string
+  editing?: boolean
+  setEditing?: Dispatch<SetStateAction<boolean>>
+  className?: string 
+}
+
+interface EditableInputProps extends EditableChildrenProps {
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function Editable({children, value, className}: EditableProps) {
+  const [editing, setEditing] = useState(false)
+
+  return (
+    <div className={className}>
+      {Children.map(children, child => 
+        isValidElement(child) && 
+        cloneElement(child as ReactElement<EditableChildrenProps>, {value, editing, setEditing})
+      )}
+    </div>
+  )
+}
+
+function EditablePreview({value, editing, setEditing, className}: EditableChildrenProps) {
+  return !editing && 
+    <span
+      onClick={() => setEditing && setEditing(true)}
+      className={className}
+    >
+      {value}
+    </span>
+}
+
+function EditableInput({value, editing, setEditing, onChange, className}: EditableInputProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    editing && inputRef.current?.select()
+  }, [editing])
+
+  return editing && 
+    <input
+      ref={inputRef}
+      type="text"
+      value={value}
+      onChange={onChange} 
+      onBlur={() => setEditing && setEditing(false)}
+      className={className}
+    />
+}
+
+Editable.Preview = EditablePreview
+Editable.Input = EditableInput
