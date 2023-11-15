9aba8cfeec21e3b767b1d65bb71905fa526af616
Duong Quoc Viet <ntysux@gmail.com>
2023-11-16 04:05:27 +0700

    add prop: onClick

--- components/menu.group/card/card.tsx
+++ components/menu.group/card/card.tsx
@@ -2,13 +2,17 @@ import { useAppSelector } from "@/redux/hooks"
 
 interface Props {
   index: number
+  onClick?: () => void
 }
 
-export default function Card({index}: Props) {
+export default function Card({index, onClick}: Props) {
   const {name, list} = useAppSelector(state => state.menuGroup)[index]
 
   return (
-    <div className="p-3 flex flex-col justify-between space-y-11 rounded-xl shadow shadow-neutral-200 hover:ring-1 hover:ring-neutral-200">
+    <div
+      onClick={onClick}
+      className="p-3 flex flex-col justify-between space-y-11 rounded-xl shadow shadow-neutral-200 hover:ring-1 hover:ring-neutral-200"
+    >
       <h3 className="truncate max-w-[100px] leading-3 text-xs text-neutral-800 font-bold">
         {name}
       </h3>
