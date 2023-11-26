aa021b4881df42fdec3fd285333fbeebf7d496c1
Duong Quoc Viet <ntysux@gmail.com>
2023-11-27 05:32:04 +0700

    debug testing

--- components/menu.group/card/card.tsx
+++ components/menu.group/card/card.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 export default function Card({index, onClick}: Props) {
-  const {name, list} = useAppSelector(state => state.menuGroup)[index]
+  const page = useAppSelector(state => state.menuGroup)[index]
 
   return (
     <div
@@ -14,10 +14,10 @@ export default function Card({index, onClick}: Props) {
       className="p-3 flex flex-col justify-between space-y-11 rounded-xl shadow shadow-neutral-200 hover:ring-1 hover:ring-neutral-200"
     >
       <h3 className="truncate max-w-[100px] leading-3 text-xs text-neutral-800 font-bold">
-        {name}
+        {page?.name}
       </h3>
       <ul className="w-fit grid grid-cols-5 gap-1">
-        {list.map((item, index) => 
+        {page?.list.map((item, index) => 
           <li key={index} className="p-1 rounded-full bg-neutral-300" />
         )}
       </ul>
