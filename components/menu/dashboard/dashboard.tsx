4d8db8a5753ac85a51814770b8141b506c2e379b
Duong Quoc Viet <ntysux@gmail.com>
2023-11-29 21:30:35 +0700

    chore: style update & add comments

--- components/menu/dashboard/dashboard.tsx
+++ components/menu/dashboard/dashboard.tsx
@@ -1,15 +1,15 @@
 'use client'
-import { IconClipboardList, IconGripHorizontal, IconRecycle, IconSwipe, IconTrash } from '@tabler/icons-react'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { checkAllDeleted, checkAll, removeMulti, restoreMulti } from '@/redux/menu/slice'
+import { Fragment } from 'react'
+import { usePathname } from 'next/navigation'
+import { Popover, Transition } from '@headlessui/react'
+import { IconGripHorizontal, IconRecycle, IconSwipe, IconTrash } from '@tabler/icons-react'
 import Colors from './colors'
+import { url } from '@/utils/app.url'
 import Checkbox from '../../checkbox'
 import CUDialog from '../crud/dialog/dialog'
-import { usePathname } from 'next/navigation'
-import { url } from '@/utils/app.url'
-import { Popover, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import CreateMenuGroup from '@/components/menu.group/crud/create.dialog'
+import { checkAllDeleted, checkAll, removeMulti, restoreMulti } from '@/redux/menu/slice'
 
 async function handleDeleteMulti(idList: string[]) {
   if (idList.length) {
@@ -29,7 +29,8 @@ async function handleRestoreMulti(idList: string[]) {
 }
 
 export default function Dashboard() {
-  const pathname = usePathname(),
+  const 
+    pathname = usePathname(),
     dispatch = useAppDispatch(),
     menu = useAppSelector(state => state.menu),
     idList = menu.filter(menu => menu.checked).map(menu => menu.id)
@@ -55,15 +56,9 @@ export default function Dashboard() {
   return (
     <div className='flex space-x-3'>
       <Popover className="relative">
-        <Popover.Button 
-          className='
-            p-3 px-9 outline-none rounded-lg shadow-custombox hover:shadow hover:shadow-neutral-200 transition-all duration-300
-            text-sm text-neutral-400 font-medium hover:text-neutral-600
-          '
-        >
+        <Popover.Button className='p-3 px-9 outline-none rounded-lg shadow-custombox hover:shadow hover:shadow-neutral-200 transition-all duration-300 text-sm text-neutral-400 font-medium hover:text-neutral-600'>
           Mới
         </Popover.Button>
-        
         <Transition
           as={Fragment}
           enter="ease-out duration-200"
@@ -78,7 +73,7 @@ export default function Dashboard() {
               {setOpen => 
                 <button 
                   onClick={() => setOpen(true)}
-                  className='p-2.5 flex justify-between items-center text-sm text-white hover:bg-neutral-600'
+                  className='p-2.5 flex justify-between items-center text-xs text-white hover:bg-neutral-600'
                 >
                   <IconSwipe size='17px' className='text-neutral-400' />
                   <span>Menu</span>
@@ -89,7 +84,7 @@ export default function Dashboard() {
               {setOpen =>
                 <button
                   onClick={() => setOpen(true)}
-                  className='p-2.5 flex justify-between items-center text-sm text-white hover:bg-neutral-600'
+                  className='p-2.5 flex justify-between items-center text-xs text-white hover:bg-neutral-600'
                 >
                   <IconGripHorizontal size='17px' className='text-neutral-400' />
                   <span>Nhóm</span>
@@ -101,6 +96,7 @@ export default function Dashboard() {
       </Popover>
       {pathname === '/menu' 
         ?
+        // Menu
         <div className='flex items-center space-x-7 px-7 rounded-lg shadow shadow-neutral-200'>
           <Checkbox
             indeterminate={indeterminate}
@@ -118,6 +114,7 @@ export default function Dashboard() {
           </div>
         </div>
         :
+        // Trash
         <div className='flex items-center space-x-7 px-7 rounded-lg shadow shadow-neutral-200'>
           <Checkbox
             indeterminate={indeterminate}
