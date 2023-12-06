a3e0b9b2a18abb84ea4f1e1c90c0785552c05c7e
Duong Quoc Viet <ntysux@gmail.com>
2023-12-07 00:47:02 +0700

    add try catch & return user

--- app/auth/api/signin/route.ts
+++ app/auth/api/signin/route.ts
@@ -2,40 +2,59 @@ import { NextResponse } from 'next/server'
 import { type NextRequest } from 'next/server'
 import { compare } from 'bcrypt'
 import { Client } from "@notionhq/client"
-import { sign } from 'jsonwebtoken'
 
 const notion = new Client({auth: process.env.NOTION_KEY})
 const notionAccountId = process.env.NOTION_ACCOUNT
 
 interface Props {
-  values: {
-    username: string
-    password: string
-  }
+  username: string
+  password: string
 }
 
 export async function POST(request: NextRequest) {
-  const {values: {username, password}}: Props = await request.json()
+  try {
+    const {username, password}: Props = await request.json()
 
-  const {results}: any = await notion.databases.query({
-    database_id: notionAccountId!,
-    filter: {
-      property: 'username',
-      rich_text: {
-        contains: username
+    // Check account on Notion by username
+    const {results}: any = await notion.databases.query({
+      database_id: notionAccountId!,
+      filter: {
+        property: 'username',
+        rich_text: {contains: username}
+      }
+    })
+
+    if (results.length) {
+      const { 
+        id,
+        properties: {
+          password: passwordReturned,
+          name,
+          verified,
+          premium
+        } 
+      } = results[0]
+
+      const passwordHashed: string = passwordReturned.rich_text[0].plain_text
+      // Compare password with password hashed
+      const passwordComparisonResults: boolean = await compare(password, passwordHashed)
+
+      if (passwordComparisonResults) {
+        return NextResponse.json({
+          user: {
+            id, 
+            name: name.rich_text[0].plain_text, 
+            verified: verified.checkbox,
+            premium: premium.checkbox
+          }
+        })
       }
     }
-  })
 
-  if (results.length) {
-    const passwordHashed: string = results[0].properties.password.rich_text[0].plain_text
-    const result: boolean = await compare(password, passwordHashed)
-
-    if (result) {
-      const {id}: {id: string} = results[0]
-      const token: string = sign({id}, process.env.SECRET_KEY!, {algorithm: 'HS256'})
-      return NextResponse.json({id}, {status: 200, headers: {'Set-Cookie': `token=${token}; Path=/`}})
-    }
+    // Message returned if user or password is wrong
+    return NextResponse.json({error: 'Tên đăng nhập hoặc mật khẩu không đúng !'}, {status: 401})
+  } catch (error: any) {
+    // Message returned if something wrong
+    return NextResponse.json({error: error.message}, {status: 500})
   }
-  return NextResponse.json({error: 'Tài khoản hoặc mật khẩu không đúng'})
 }
