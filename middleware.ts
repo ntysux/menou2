cd5dcc98999eda8afb7889abda2b90344d479547
Duong Quoc Viet <ntysux@gmail.com>
2023-12-07 01:58:42 +0700

    stop checking token

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,10 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { url } from './utils/app.url'
 
 export default function Middleware(request: NextRequest) {
-  const cookie = request.cookies.get('token')
-
-  if (cookie) {
-    return NextResponse.redirect(`${url}/menu`)
-  }
-  
   return NextResponse.next()
 }
 
-export const config = {
-  matcher: ['/']
-}
+// export const config = {
+//   matcher: ['/']
+// }
