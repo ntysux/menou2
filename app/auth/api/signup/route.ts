528cc8a09c5459afa521e5ad778def738f34d27e
Duong Quoc Viet <ntysux@gmail.com>
2023-12-07 18:14:31 +0700

    handle create new account if signin with google

--- app/auth/api/signup/route.ts
+++ app/auth/api/signup/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server'
 import { type NextRequest } from 'next/server'
-import jwt from 'jsonwebtoken'
 import { genSalt, hash } from 'bcrypt'
 import { Client as NotionClient } from "@notionhq/client"
 
@@ -8,66 +7,115 @@ const notion = new NotionClient({auth: process.env.NOTION_KEY})
 const notionAccountId = process.env.NOTION_ACCOUNT
 
 interface Props {
-  values: {
-    username: string
-    password: string
-    name: string
-  }
+  username?: string
+  password?: string
+  name: string
+  email?: string
 }
 
 export async function POST(request: NextRequest) {
-  const {values: {username, password, name}}: Props = await request.json()
+  try {
+    const {username, password, name, email}: Props = await request.json()
 
-  const {results} = await notion.databases.query({
-    database_id: notionAccountId!,
-    filter: {
-      property: 'username',
-      rich_text: {
-        contains: username
+    // Google
+    if (email) {
+      const {results} = await notion.databases.query({
+        database_id: notionAccountId!,
+        filter: {
+          property: 'email',
+          email: {
+            contains: email
+          }
+        }
+      })
+
+      // Return id if account already exist
+      if (results.length) { 
+        return NextResponse.json({id: results[0].id})
+      } 
+
+      // Create new account & return id if account doesn't exist
+      else {
+        const {id} = await notion.pages.create({
+          parent: {
+            database_id: notionAccountId!
+          },
+          properties: {
+            name: {
+              rich_text: [{
+                text: {
+                  content: name
+                }
+              }]
+            },
+            email: {
+              email
+            }
+          }
+        })
+
+        return NextResponse.json({id})
+      }
+    } 
+    
+    // Credentials
+    else {
+      const {results} = await notion.databases.query({
+        database_id: notionAccountId!,
+        filter: {
+          property: 'username',
+          rich_text: {
+            contains: username!
+          }
+        }
+      })
+
+      // Error returned if username already exists
+      if(results.length) {
+        return NextResponse.json({error: 'Tên đăng nhập đã tồn tại !'})
+      } 
+
+      // Create new account & return id if account doesn't exist
+      else {
+        // Hash password
+        const salt: string = await genSalt(10)
+        const passwordHashed: string = await hash(password!, salt)
+
+        // Create account
+        const {id} = await notion.pages.create({
+          parent: {
+            database_id: notionAccountId!
+          },
+          properties: {
+            username: {
+              title: [{
+                text: {
+                  content: username!
+                }
+              }]
+            },
+            password: {
+              rich_text: [{
+                text: {
+                  content: passwordHashed
+                }
+              }]
+            },
+            name: {
+              rich_text: [{
+                text: {
+                  content: name
+                }
+              }]
+            }
+          }
+        })
+
+        return NextResponse.json({id})
       }
     }
-  })
-  
-  // return if username already exists
-  if(results.length) {
-    return NextResponse.json({username: 'Tên đăng nhập đã tồn tại.'})
-  } else {
-    // hash password with Bcrypt
-    const salt: string = await genSalt(10)
-    const passwordHashed: string = await hash(password, salt)
-
-    // store account into Notion
-    const {id} = await notion.pages.create({
-      parent: {
-        database_id: notionAccountId!
-      },
-      properties: {
-        username: {
-          title: [{
-            text: {
-              content: username
-            }
-          }]
-        },
-        password: {
-          rich_text: [{
-            text: {
-              content: passwordHashed
-            }
-          }]
-        },
-        name: {
-          rich_text: [{
-            text: {
-              content: name
-            }
-          }]
-        }
-      }
-    })
-
-    // create token
-    const token: string = jwt.sign({id}, process.env.SECRET_KEY!, {algorithm: 'HS256'})
-    return NextResponse.json({id}, {status: 200, headers: {'Set-Cookie': `token=${token}; Path=/`}})
+  } catch (error: any) {
+    // Return error if something wrong
+    return NextResponse.json({error: error.message}, {status: 500})
   }
 }
