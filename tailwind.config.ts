ba14a5348a8a5c664d289fb849211e27d538a358
Duong Quoc Viet <ntysux@gmail.com>
2023-12-16 06:55:21 +0700

    chore: update shadow style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         'hero-pattern': `url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%230a0a0a' fill-opacity='0.75' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");`,
       },
       boxShadow: {
-        'custombox': '-10px -10px 15px rgba(255,255,255,0.5),10px 10px 15px rgba(70,70,70,0.12);'
+        'custombox': 'rgba(231, 229, 228, 0.95) 0px 15px 25px, rgba(231, 229, 228, 0.75) 0px 5px 10px;'
       }
     },
   },
